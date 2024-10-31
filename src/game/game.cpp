c939165fed3b1e55c3be28a43939027deb8c4eb5
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2024-10-31 09:10:30 -0300

    update

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -202,11 +202,7 @@ void Game::drawMoves(shared_ptr<Player> &player) const
     auto movePairs = filterMovesByType(pokemon, moves);
     vector<Move *> validTypeMoves = movePairs.first;
 
-    cout << "Moves: " << validTypeMoves.size() << endl;
-
     vector<Move *> normalMoves = movePairs.second;
-
-    cout << "Normal Moves: " << normalMoves.size() << endl;
 
     // Sorteia até 4 moves para o Pokémon
     for (int i = 0; i < 4; i++)
